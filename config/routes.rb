0603dee34df32c7a4079f5231e73145b95bf1e1e
Paul Thompson <paulthompson1@gmail.com>
2016-11-23 13:22:22 +1100

    updated routes rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :activities
   resources :services
   resources :businesses
-  resources :service_types
+  # resources :service_types
   resources :profiles
   root 'pages#home'
   get 'about' => 'pages#about'
